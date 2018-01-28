f610c83368224489bfb30935272c878d788b7de7
w-smith <wsmith@bex.net>
2018-01-28 13:00:20 -0700

    adding components

--- valkyrie/src/app/app.module.ts
+++ valkyrie/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
