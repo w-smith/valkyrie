df2f61c3500611cb9c5f16d79e3d1fb39d26fbc7
w-smith <wsmith@bex.net>
2018-01-28 17:32:07 -0700

    fucking up api search functionality

--- valkyrie/src/app/search/search.component.ts
+++ valkyrie/src/app/search/search.component.ts
@@ -10,6 +10,24 @@ export class SearchComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+
+ //  	const igdb = require('igdb-api-node').default;
+	// const client = igdb("484040fd12c1e28f0f3cf8e1de7c07ad");
+
+
+	// client.games({
+ //    	fields: '*', 
+ //    	limit: 5, 
+ //    	offset: 15 
+	// }).then(response => {
+		
+	// }).catch(error => {
+ //  	  throw error;
+	// });
+
+
+
   }
 
 }
