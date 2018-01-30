fe908a95f659a03b4ee79520dabeacf0a2e0fc83
w-smith <wsmith@bex.net>
2018-01-29 18:08:40 -0700

    messing with API call

--- valkyrie/src/app/search/search.component.ts
+++ valkyrie/src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -11,23 +12,12 @@ export class SearchComponent implements OnInit {
 
   ngOnInit() {
 
-
- //  	const igdb = require('igdb-api-node').default;
-	// const client = igdb("484040fd12c1e28f0f3cf8e1de7c07ad");
-
-
-	// client.games({
- //    	fields: '*', 
- //    	limit: 5, 
- //    	offset: 15 
-	// }).then(response => {
-		
-	// }).catch(error => {
- //  	  throw error;
-	// });
-
+$("#searchBtn").click(function(){
+	console.log("anus bros");
+});
 
 
   }
+
 
 }
