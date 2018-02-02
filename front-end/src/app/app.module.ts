29e84e72d89134404054cadca19358f350321961
w-smith <wsmith@bex.net>
2018-02-01 17:08:02 -0700

    firebase setup

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
@@ -8,7 +8,17 @@ import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SearchComponent } from './search/search.component';
 import * as $ from 'jquery'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBNSycBUmjHmr76tMGF3oB8gJgcyQy5KDE",
+    authDomain: "valkyrie-f6f58.firebaseapp.com",
+    databaseURL: "https://valkyrie-f6f58.firebaseio.com",
+    projectId: "valkyrie-f6f58",
+    storageBucket: "valkyrie-f6f58.appspot.com",
+    messagingSenderId: "939030606986"
+}
 
 @NgModule({
   declarations: [
@@ -19,9 +29,19 @@ import * as $ from 'jquery'
     SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+
+
+}
+
+
--- front-end/src/app/signup/signup.component.ts
+++ front-end/src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import { formdiv } from './signup.component.html';
+// import {  } from 'firebase';
+
 
 @Component({
   selector: 'app-signup',
@@ -10,6 +13,53 @@ export class SignupComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
 
+
+// var config = {
+//     apiKey: "AIzaSyBNSycBUmjHmr76tMGF3oB8gJgcyQy5KDE",
+//     authDomain: "valkyrie-f6f58.firebaseapp.com",
+//     databaseURL: "https://valkyrie-f6f58.firebaseio.com",
+//     projectId: "valkyrie-f6f58",
+//     storageBucket: "valkyrie-f6f58.appspot.com",
+//     messagingSenderId: "939030606986"
+//   };
+//   firebase.initializeApp(config);
+
+// var userRef = firebase.database().ref('user');
+
+// // listen for form submission
+// document.getElementById('formdiv').addEventListener('Submit', submitForm);
+
+// // submit form
+// function submitForm(event){
+// 	event.preventDefault();
+// 	console.log("submit test");
+
+// // get values
+// var email = getInputVal('email');
+// var password = getInputVal('password');
+// var repeatpassword = getInputVal('repeatpassword');
+
+// // function to get form values
+
+// function getInputVal(id){
+// 	return (<HTMLInputElement>document.getElementById(id)).value;
+// }
+
+
+// function saveUser(email, password, repeatpassword){
+// 	var newUserRef = userRef.push();
+// 	newUserRef.set ({
+// 		email : email,
+// 		password : password,
+// 		reapeatpassword : repeatpassword
+// 	})
+// }
+
+
+
+// 		}
+
+
+	}
 }
