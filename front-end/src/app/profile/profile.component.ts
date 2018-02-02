42305b6bfab28dbc03ce63d27becf079a035a1f9
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-02-02 14:47:32 -0700

    edit buttons

--- front-end/src/app/profile/profile.component.ts
+++ front-end/src/app/profile/profile.component.ts
@@ -8,34 +8,47 @@ import { AuthService } from '../auth.service';
 })
 export class ProfileComponent implements OnInit {
 
-<<<<<<< HEAD
 
-
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
-	
-	document.getElementById("psnpencil").addEventListener("click", function(){
-    	document.getElementById("psnplaceholder").setAttribute("contenteditable", "true");
-    });
+    document.getElementById("psnpencil").addEventListener("click", function(){
+      var x =  document.getElementById("psnplaceholder");
+      x.setAttribute("contenteditable", "true");
+      x.focus();
+     });
+
+    document.getElementById("xboxpencil").addEventListener("click", function(){
+      var x =  document.getElementById("xboxplaceholder");
+      x.setAttribute("contenteditable", "true");
+      x.focus();
+     });
+
+    document.getElementById("bnetpencil").addEventListener("click", function(){
+      var x =  document.getElementById("bnetplaceholder");
+      x.setAttribute("contenteditable", "true");
+      x.focus();
+     });
+
+    document.getElementById("steampencil").addEventListener("click", function(){
+      var x =  document.getElementById("steamplaceholder");
+      x.setAttribute("contenteditable", "true");
+      x.focus();
+     });
+
+    document.getElementById("riotpencil").addEventListener("click", function(){
+      var x =  document.getElementById("riotplaceholder");
+      x.setAttribute("contenteditable", "true");
+      x.focus();
+     });
 
 
   }
-
-
-
-
-
-
-=======
-  constructor(public authService: AuthService) { }
-
-  ngOnInit() {}
 
   signOut() {
     this.authService.logout();
   }
->>>>>>> fe0245be961e26d7b559ae3037ee9dd5eb0be55c
+
 }
 
 
